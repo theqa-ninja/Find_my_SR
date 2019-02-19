6c30dff32028fd49c8bc8da8a1afd4bc0c7de1e2
David Tan <dtan@instructure.com>
2019-02-19 08:56:48 -0600

    reusable code!

--- borderify.js
+++ borderify.js
@@ -1,33 +1,26 @@
+tagList = ["a", "input", "ScreenReader", "img"]
+attributeList = ["[aria-label]", "[screenreader-only]"]
+mustContainList = ["screenreader-only", "alt", "aria-label"]
+
+
 document.body.style.border = "1px solid red";
 console.log("big border placed");
 
-var links = document.getElementsByTagName("a");
-console.log("Found "+links.length + " links");
-for(var i=0;i<links.length;i++)
+for(var n = 0; n < tagList.length; n++)
 {
-  if(links[i].href)
+  var tagsFound = document.getElementsByTagName(tagList[n]);
+  console.log("Found "+ tagsFound.length + " of tag: " + tagList[n]);
+  for(var i=0;i<tagsFound.length;i++)
   {
-    links[i].style.border = "1px solid #00FF00";
+    tagsFound[i].style.border = "1px solid #00FF00";
   }
 }
 
-
-var inputList = document.getElementsByTagName("input");
-console.log("Found "+ inputList.length + " inputs");
-// console.log("hi mom!");
-for(var i=0;i<inputList.length;i++)
+for(var n = 0; n < attributeList.length; n++)
 {
-  if(inputList[i].href)
-  {
-    inputList[i].style.border = "1px solid #FF0000";
-  }
-}
-
-var inputs = document.querySelectorAll("[aria-label]");
-console.log("Found "+ inputs.length + " blah");
-for(var i=0;i<inputs.length;i++)
-{
-  if(inputs[i].href)
+  var inputs = document.querySelectorAll(attributeList[n]);
+  console.log("Found "+ inputs.length + " attribute: " + attributeList[n]);
+  for(var i=0;i<inputs.length;i++)
   {
     inputs[i].style.border = "1px solid #0000FF";
   }
